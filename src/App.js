ac8d127024f02b862f037aadf214bd2d4db99a25
Sidhartha Chatterjee <me@sidharthachatterjee.com>
2018-10-27 15:28:32 +0530

    Reactor to use useState and remove classes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,58 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import uuidv4 from 'uuid/v4'
 
 import Todo from './Todo'
 
-class App extends Component {
-  state = {
-    inputValue: '',
-    todos: {}
+const App = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState({})
+
+  const addTodo = todo => {
+    setInputValue('')
+    setTodos({
+      ...todos,
+      [uuidv4()]: todo
+    })
   }
 
-  addTodo = todo => {
-    this.setState({
-      inputValue: '',
-      todos: {
-        ...this.state.todos,
-        [uuidv4()]: todo
+  const toggleTodo = id => {
+    setTodos({
+      ...todos,
+      [id]: {
+        ...todos[id],
+        checked: !todos[id].checked
       }
     })
   }
 
-  toggleTodo = id => {
-    this.setState({
-      todos: {
-        ...this.state.todos,
-        [id]: {
-          ...this.state.todos[id],
-          checked: !this.state.todos[id].checked
-        }
-      }
-    })
-  }
-
-  onKeyDownHandler = event => {
+  const onKeyDownHandler = event => {
     if (event.keyCode === 13) {
-      this.addTodo({
-        value: this.state.inputValue,
+      addTodo({
+        value: inputValue,
         checked: false
       })
     }
   }
 
-  onChange = event => {
-    this.setState({
-      inputValue: event.target.value
-    })
-  }
+  const onChange = event => setInputValue(event.target.value)
 
-  render() {
-    const { inputValue, todos } = this.state
-    return (
-      <main className="app container mx-auto">
-        <h1>To Do</h1>
-        <input
-          value={inputValue}
-          placeholder="Add a task"
-          className="todo-input"
-          onKeyDown={this.onKeyDownHandler}
-          onChange={this.onChange}
-        />
-        <ul className="list-reset mt-4 w-full">
-          {Object.keys(todos).map(id => (
-            <Todo id={id} todo={todos[id]} toggleTodo={this.toggleTodo} />
-          ))}
-        </ul>
-      </main>
-    )
-  }
+  return (
+    <main className="app container mx-auto">
+      <h1>To Do</h1>
+      <input
+        value={inputValue}
+        placeholder="Add a task"
+        className="todo-input"
+        onKeyDown={onKeyDownHandler}
+        onChange={onChange}
+      />
+      <ul className="list-reset mt-4 w-full">
+        {Object.keys(todos).map(id => (
+          <Todo id={id} todo={todos[id]} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </main>
+  )
 }
 
 export default App
